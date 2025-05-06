145a5f8df864f9f96511900d5bb670f919d032f5
George <george.boujhenitsa@gmail.com>
2025-05-06 18:27:57 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,25 +5,31 @@ const body = document.body;
 
 function firstPromise() {
   return new Promise((resolve, reject) => {
-    pageDocument.addEventListener('mousedown', (e) => {
+    const handler = (e) => {
       if (e.button === 0) {
         resolve('First promise was resolved');
       }
-    });
+    };
+
+    pageDocument.addEventListener('mouseover', handler);
 
     setTimeout(() => {
       reject(new Error());
+      pageDocument.removeEventListener('mouseover', handler);
     }, 3000);
   });
 }
 
 function secondPromise() {
   return new Promise((resolve) => {
-    pageDocument.addEventListener('mousedown', (e) => {
+    const handler = (e) => {
       if (e.button === 0 || e.button === 2) {
         resolve('Second promise was resolved');
+        pageDocument.removeEventListener('mousedown', handler);
       }
-    });
+    };
+
+    pageDocument.addEventListener('mousedown', handler);
   });
 }
 
